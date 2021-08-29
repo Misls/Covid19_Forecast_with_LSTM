549ca89b965b98fe462428bb50f864c04a3c0152
Tino <meisel.tino@t-online.de>
2021-08-29 19:45:25 +0200

    merged weekly and daily data (hospitalisierung, fallzahl) and interpolatd missing data with spline

--- Covid19_Forecast.py
+++ Covid19_Forecast.py
@@ -21,7 +21,7 @@ from isoweek import Week
 
 #data from RKI github repository:
 
-#data_RWert = pd.read_csv('https://github.com/robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/blob/0c3a7b18078ca50f81b9002976801bb826a77197/Nowcast_R_aktuell.csv?raw=true')
+data_RWert = pd.read_csv('https://github.com/robert-koch-institut/SARS-CoV-2-Nowcasting_und_-R-Schaetzung/blob/0c3a7b18078ca50f81b9002976801bb826a77197/Nowcast_R_aktuell.csv?raw=true')
 #data_infections = pd.read_csv('https://github.com/robert-koch-institut/SARS-CoV-2_Infektionen_in_Deutschland/blob/master/Aktuell_Deutschland_SarsCov2_Infektionen.csv?raw=true')
 #data_vaccinations = pd.read_csv('https://github.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/blob/master/Aktuell_Deutschland_Bundeslaender_COVID-19-Impfungen.csv?raw=true')
 #data_VOC = pd.read_csv('https://github.com/robert-koch-institut/SARS-CoV-2-Sequenzdaten_aus_Deutschland/raw/master/SARS-CoV-2-Sequenzdaten_Deutschland.csv.xz')
@@ -30,37 +30,38 @@ from isoweek import Week
 
 r = requests.get('https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Klinische_Aspekte.xlsx?__blob=publicationFile', headers={"User-Agent": "Chrome"})
 data_all = pd.read_excel(BytesIO(r.content),header=2)
-print(np.mean(data_all['Anteil Verstorben']))
-for col in data_all.columns:
-   print(col)
+
 
 
 ############################# data preprocessing ####################
 
 # convert a week into a date (aim: inperpolate between the weekly data to get daily data)
-
-
-def process(data_all):
+def process(df,year_key,week_key,day):
     dates = []
     for i in range(len(data_all['MW'])):
         date = Week(
-            int(data_all.loc[i, 'Meldejahr']),
-            int(data_all.loc[i, 'MW'])
-            ).thursday()
-        dates.append(date.isoformat())
-    return dates
+            int(df.loc[i, year_key]),           # year values
+            int(df.loc[i, week_key])            # week values
+            ).day(day)
+        dates.append(date.isoformat())          # create date in isoformat
+    return np.reshape(dates, (1, len(dates))).T # vector of dates
+
+# insert date values in current dataframe
+data_all['Datum'] = process(data_all,'Meldejahr','MW',3)
+print(data_all.head(5))
+
+# merge dataframes on 'Datum' and interpolate missing data in data_all
+data_merged = pd.merge(data_RWert,data_all,how='outer',on=['Datum'])
+data = data_merged.interpolate(method= 'spline',order=5).dropna()
 
 
-
-#data_all = data_all.drop(index=43)
-print(data_all[['MW']].head(50))
-
-dates = process(data_all)
-print(dates)
 
 #print(data_all.loc[data_all['Meldejahr'] == 2021, 'MW'])
 #x = data_all.loc[data_all['Meldejahr'] == 2021, 'MW']
 #y = data_all.loc[data_all['Meldejahr'] == 2021, 'Anzahl hospitalisiert']
-#plt.plot(x,y)
-#plt.show()
+x = data['Datum']
+y = data['Anzahl hospitalisiert']
+plt.plot(x,data['PS_COVID_Faelle'])
+plt.plot(x,data['Fälle gesamt']/7)
+plt.show()
 
